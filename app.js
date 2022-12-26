649595e6d18522fb2d98309cec59588ed5ba0b68
sofy sofya <sofy7468612@gmail.com>
2022-12-26 13:00:34 +0530

    sdfghjjnhbvcxsdfgh

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express();
 
 const publicRoute = require('./routes/public');
+const privateRoute = require("./routes/private");
 const adminRoute = require('./routes/admin');
 const AuthCheck = require("./middlewares/AuthCheck");
 const AdminCheck = require("./middlewares/AdminCheck");
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/public',publicRoute);
+app.use("/api/private", AuthCheck, privateRoute);
 app.use('/api/admin',AuthCheck,AdminCheck,adminRoute);
 
 //test api
